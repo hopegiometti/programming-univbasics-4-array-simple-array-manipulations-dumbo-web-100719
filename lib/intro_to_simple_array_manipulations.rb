924ef98d4bc499415ec752465ce9281916ad294d
Hope Giometti <hopegiometti@gmail.com>
2019-09-19 16:54:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -24,3 +24,6 @@ def using_shift(array)
   @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
   @im_so_over_this_city = @my_favorite_cities.shift
 end
+
+def shift_with_args(array)
+end
