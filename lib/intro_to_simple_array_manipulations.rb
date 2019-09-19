ae595b271d66b3066a2760b4727a26732e07a3d6
Hope Giometti <hopegiometti@gmail.com>
2019-09-19 16:57:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,5 +33,5 @@ end
 def using_concat(array1, array2)
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
-  @all_my_favs = @my
+  @all_my_favs = @my_favorite_things.conat(more_favs)
 end
