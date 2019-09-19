88c4e462ff0664f1a7e1f8082d7f8c7368eab4db
Hope Giometti <hopegiometti@gmail.com>
2019-09-19 16:57:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,4 +31,7 @@ def shift_with_args(array)
 end
 
 def using_concat(array1, array2)
+  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  @all_my_favs = @my
 end
